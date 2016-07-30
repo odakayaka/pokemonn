afc01bd0f2c2bcbd5496d8e5ad1f12ab8b6dc105
odakayaka <ayaka.odaka@gmail.com>
2016-07-30 09:45:34 +0900

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by 尾高文香 on 2016/07/30.
+//  Copyright © 2016年 com.odakaayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
